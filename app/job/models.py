474c0e1b1124c0172a6e79a584297816baa25d53
beni-f <benifissha@gmail.com>
2025-02-28 09:26:59 +0300

    updated

--- app/job/models.py
+++ app/job/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+    role = models.CharField(choices=ROLE_CHOICES, default='user', max_length=10)
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=16)
+    
