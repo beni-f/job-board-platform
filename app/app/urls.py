bb4e0439d03ee478cacc0eed8dc890f0ceb6eeef
beni-f <benifissha@gmail.com>
2025-03-12 22:53:34 +0300

    updated urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
